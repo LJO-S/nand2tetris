69c8c15c8ca6433bfeca68f2d638aa8c9516c6af
Ludvig Snihs <ludvig.snihs@outlook.com>
2025-01-17 21:50:25 +0100

    Add functionality for Mult.asm

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,36 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// A multiplication R0 * R1 is simply R1 + R1 + R1 ... R0 times
+    @sum
+    M=0
+    @i
+    M=0
+    @R2
+    M=0
+(LOOP) // Loop over R1 until D=R0 is smaller than 0
+    @i
+    D=M
+    @R0
+    D=D-M
+    @OUTPUT
+    D;JEQ
+    @R1
+    D=M
+    @sum
+    M=M+D
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+(OUTPUT) // R2 output
+    @sum
+    D=M
+    @R2
+    M=D
+(END)
+    @END
+    0;JMP
+
+
+
