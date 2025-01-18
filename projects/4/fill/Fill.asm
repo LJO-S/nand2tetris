e00485c0440510ca3e361a87b643f010ff62102a
Ludvig Snihs <ludvig.snihs@outlook.com>
2025-01-18 11:27:33 +0100

    Tidy up Fill.asm to look a little more elegant

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,59 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+(INIT)
+    @SCREEN
+    D=A
+    // save addr of screen base
+    @addr 
+    M=D
+    // check keystroke
+    @24576 
+    D=M
+    // save keystroke
+    @last_keystroke
+    M=D
+(LOOP_1)
+    // check if current != previous keystroke
+    @24576
+    D=M
+    @last_keystroke
+    D=D-M
+    @INIT
+    D;JNE
+    // check keystroke
+    @24576
+    D=M
+    @WHITE
+    D;JEQ
+    @BLACK
+    D;JMP
+(LOOP_2)
+    // incr address
+    @addr
+    M=M+1
+    // check if we are at end of screen RAM
+    D=M
+    @24575
+    D=D-A
+    @INIT
+    D;JGT
+    @LOOP_1
+    D;JMP
+(WHITE)
+    @addr
+    A=M
+    // set to 0x0000
+    M=0
+    @LOOP_2
+    D;JMP
+(BLACK)
+    @addr
+    A=M
+    // set to 0xFFFF
+    M=-1
+    @LOOP_2
+    D;JMP
+
+
+
