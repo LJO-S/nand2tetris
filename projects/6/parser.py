153d65a68f305f3d65839c5b34706d25c9d8153f
Ludvig Snihs <ludvig.snihs@outlook.com>
2025-01-26 21:55:55 +0100

    Initial issue

---
+++ projects/6/parser.py
@@ -0,0 +1,18 @@
+
+class Parser:
+    def __init__(self, path):
+        self.path = path
+        
+        self.OPEN_FILE(path)
+
+    def __str__(self):
+        print("This is a test!")
+
+    def open_input_file(self):
+        # bla bla bla
+
+
+
+
+if __name__ == "__main__":
+    print("Hello world!")
