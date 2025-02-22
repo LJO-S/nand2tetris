e43488c5526203c7980dd0f37d2a984bd5a18de6
Ludvig Snihs <ludvig.snihs@outlook.com>
2025-02-22 13:36:56 +0100

    Add support for single file and directory parsing

--- projects/7/main.py
+++ projects/7/main.py
@@ -3,21 +3,8 @@ from parser import Parser
 import sys
 from pathlib import Path
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python main.py [input_dir/input_filename] > [output_filename]")
-        sys.exit(1)
-    # Get filename from cmd line
-    input_path = sys.argv[1]
-    input_path = Path(input_path)
-    output_dir = input_path.parent
-    output_name = output_dir.name
-    output_file = str(output_dir) + "/" + f"{output_name}.asm"
 
-    parser = Parser(input_path)
-    coder = Code(output_file)
-    coder.setFileName(output_name)
-
+def parserCoder(parser: Parser, coder: Code):
     while parser.hasMoreCommands() == True:
         parser.advance()
         if parser.commandType() == "C_PUSH":
@@ -36,6 +23,37 @@ if __name__ == "__main__":
             coder.writeArithmetic(parser.arg1())
         else:
             print("Unexpected commandType at this stage!!" + parser.commandType())
-
     parser.close_input_file()
     coder.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python main.py [input_dir/input_filename.vm] or [input_dir]")
+        sys.exit(1)
+    # Get filename from cmd line
+    input_path = sys.argv[1]
+    input_path = Path(input_path)
+    if "vm" in str(input_path).split("."):
+        # Single file
+        output_dir = input_path.parent
+        output_name = output_dir.name
+        output_file = str(output_dir) + "/" + f"{output_name}.asm"
+        parser = Parser(input_path)
+        coder = Code(output_file)
+        coder.setFileName(input_path.stem)
+        parserCoder(parser, coder)
+        parser.close_input_file()
+        coder.close()
+    else:
+        # Directory (multiple files)
+        output_dir = input_path
+        output_name = output_dir.name
+        output_file = str(output_dir) + "/" + f"{output_name}.asm"
+        coder = Code(output_file)
+        for vm_file in input_path.glob("*.vm"):
+            parser = Parser(vm_file)
+            coder.setFileName(vm_file.stem)
+            parserCoder(parser, coder)
+            parser.close_input_file()
+        coder.close()
