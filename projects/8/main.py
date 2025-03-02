1af250190feddcf833dd82bd8df54dad113fb6fa
Ludvig Snihs <ludvig.snihs@outlook.com>
2025-03-02 22:09:27 +0100

    Further work on more advanced functionality. Remove single file parsing

--- projects/8/main.py
+++ projects/8/main.py
@@ -5,9 +5,8 @@ from pathlib import Path
 
 
 def parserCoder(parser: Parser, coder: Code):
-    # TODO: add support for rest of C_... commands
-
-    # TODO: in label, check if we're in a function. If so, see functionName$label
+    isFunction = False
+    nameFunction = None
     while parser.hasMoreCommands() == True:
         parser.advance()
         if parser.commandType() == "C_PUSH":
@@ -24,6 +23,32 @@ def parserCoder(parser: Parser, coder: Code):
             )
         elif parser.commandType() == "C_ARITHMETIC":
             coder.writeArithmetic(parser.arg1())
+        elif parser.commandType() == "C_CALL":
+            coder.writeCall(parser.arg1(), int(parser.arg2()))
+        elif parser.commandType() == "C_LABEL":
+            if isFunction == True:
+                # Nested in function
+                coder.writeLabel(nameFunction + "$" + parser.arg1())
+            else:
+                # Standalone
+                coder.writeLabel(parser.arg1())
+        elif parser.commandType() == "C_GOTO":
+            if isFunction == True:
+                coder.writeGoto(nameFunction + "$" + parser.arg1())
+            else:
+                coder.writeGoto(parser.arg1())
+        elif parser.commandType() == "C_IF":
+            if isFunction == True:
+                coder.writeIf(nameFunction + "$" + parser.arg1())
+            else:
+                coder.writeIf(parser.arg1())
+        elif parser.commandType() == "C_FUNCTION":
+            isFunction = True
+            nameFunction = parser.arg1()
+            coder.writeFunction(parser.arg1(), int(parser.arg2()))
+        elif parser.commandType() == "C_RETURN":
+            coder.writeReturn()
+
         else:
             print("Unexpected commandType at this stage!!" + parser.commandType())
     parser.close_input_file()
@@ -32,33 +57,24 @@ def parserCoder(parser: Parser, coder: Code):
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: python main.py [input_dir/input_filename.vm] or [input_dir]")
+        print("Usage: python main.py [input_dir]")
         sys.exit(1)
     # Get filename from cmd line
     input_path = sys.argv[1]
     input_path = Path(input_path)
-    if "vm" in str(input_path).split("."):
-        # Single file
-        output_dir = input_path.parent
-        output_name = output_dir.name
-        output_file = str(output_dir) + "/" + f"{output_name}.asm"
-        parser = Parser(input_path)
-        coder = Code(output_file)
-        coder.setFileName(input_path.stem)
+    # Directory (multiple files)
+    output_dir = input_path
+    output_name = output_dir.name
+    output_file = str(output_dir) + "/" + f"{output_name}.asm"
+    coder = Code(output_file)
+    if not "SimpleFunction" in output_name:
+        if "Sys.vm" in input_path.glob("*.vm"):
+            coder.writeInit(True)
+        else:
+            coder.writeInit(False)
+    for vm_file in input_path.glob("*.vm"):
+        parser = Parser(vm_file)
+        coder.setFileName(vm_file.stem)
         parserCoder(parser, coder)
         parser.close_input_file()
-        coder.close()
-    else:
-        # Directory (multiple files)
-        # TODO: need to write [sys.vm --> .asm] to the first lines
-        # in output!!!!!!!
-        output_dir = input_path
-        output_name = output_dir.name
-        output_file = str(output_dir) + "/" + f"{output_name}.asm"
-        coder = Code(output_file)
-        for vm_file in input_path.glob("*.vm"):
-            parser = Parser(vm_file)
-            coder.setFileName(vm_file.stem)
-            parserCoder(parser, coder)
-            parser.close_input_file()
-        coder.close()
+    coder.close()
