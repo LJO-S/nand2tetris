188caeb9d26285ccdd1337a359aae9813c8754a0
Ludvig Snihs <ludvig.snihs@outlook.com>
2025-04-04 20:21:59 +0200

    Add support for tokenizing single file inputs

--- projects/10/JackTokenizer.py
+++ projects/10/JackTokenizer.py
@@ -1,3 +1,8 @@
+import re
+import sys
+from pathlib import Path
+
+
 class JackTokenizer:
     """The Jack Tokenizer's purpose is to read
     a .jack file (or directory) and strip all
@@ -6,8 +11,18 @@ class JackTokenizer:
     is an .xml containing all tokens."""
 
     def __init__(self, inputFile=None):
-        self.file = self.open_output_file(inputFile)
+        self.file = self.open_input_file(inputFile)
         self.currentToken = None
+        self.tokens = None
+
+    def open_input_file(self, path: str):
+        """Opens the file specified by path"""
+        file = open(path, "r")
+        return file
+
+    def close_input_file(self):
+        """Closes input file"""
+        self.file.close()
 
     def hasMoreTokens(self):
         """Checks if the current line is EOF or not
@@ -19,11 +34,9 @@ class JackTokenizer:
         line_split = line.split()
         while line == "\n" or (
             len(line_split) > 0
-            and (longComment)
-            or ("//" in line_split[0])
-            or ("/**" in line_split[0])
+            and ((longComment) or ("//" in line_split[0]) or ("/**" in line_split[0]))
         ):
-            if "/**" in line_split[0]:
+            if (len(line_split) > 0) and "/**" in line_split[0]:
                 longComment = True
             if "*/" in line_split:
                 longComment = False
@@ -37,7 +50,16 @@ class JackTokenizer:
             return False
 
     def advance(self):
-        pass
+        """Advances by reading the first word in currentLine and
+        popping that word"""
+        if self.tokens == None or len(self.tokens) == 0:
+            line = self.file.readline()
+            # Substitute // and all following characters with  using Regular Expression
+            line_no_comments = re.sub(r"//.*", " ", line)
+            # Find all word characters (a-z, 0-9) that occur at least once or...
+            # ... all non-whitespace and non-word i.e. symbols.
+            self.tokens = re.findall(r"\w+|[^\s\w]", line_no_comments)
+        self.currentToken = self.tokens.pop(0)
 
     def tokenType(self):
         pass
@@ -59,4 +81,27 @@ class JackTokenizer:
 
 
 if __name__ == "__main__":
-    print("Hello world!\r\n")
+    # TODO modify this to handle .xml files
+    if len(sys.argv) < 2:
+        print(
+            "Usage: python JackTokenizer.py [input_dir/input_filename.vm] or [input_dir]"
+        )
+        sys.exit(1)
+    # Get filename from cmd line
+    input_path = sys.argv[1]
+    input_path = Path(input_path)
+    if "jack" in str(input_path).split("."):
+        # Single file
+        tokenizer = JackTokenizer(input_path)
+        while tokenizer.hasMoreTokens():
+            tokenizer.advance()
+            print(tokenizer.currentToken)
+        tokenizer.close_input_file()
+    else:
+        # Directory (multiple files)
+        for vm_file in input_path.glob("*.jack"):
+            tokenizer = JackTokenizer(vm_file)
+            while tokenizer.hasMoreTokens():
+                tokenizer.advance()
+                print(tokenizer.currentToken)
+            tokenizer.close_input_file()
