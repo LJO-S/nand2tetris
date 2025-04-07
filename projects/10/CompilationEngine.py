9e69c441ea414f509465dc8b489080fb112f0ad0
Ludvig Snihs <ludvig.snihs@outlook.com>
2025-04-07 22:24:12 +0200

    Add initial architecture of compileClass method. Uses a list of ET.SubElements pointing at earlier index to get recursiveness

--- projects/10/CompilationEngine.py
+++ projects/10/CompilationEngine.py
@@ -4,29 +4,134 @@ import xml.etree.ElementTree as ET
 import re
 import sys
 from pathlib import Path
+import JackTokenizer
 
-
-# Let JackAnalyzer feed the JackTokenizer into the CompilationEngine, no xml_T needed...
-# The CompilationEngine will then be the master, handling .advance() and so on.
+# This will be the master, handling .advance() and so on.
+# Only .advance() if .hasMoreTokens() is true
 # Terminals: keyword, symbol, integerConst, strinGconstant, indetifier
+
+
+# TODO
+# ParameterList or varDec
 
 
 class CompilationEngine:
 
-    def __init__(self, inputPath=None, outputPath=None):
-        pass
+    def __init__(self, tokenizer: JackTokenizer, outputFile: str):
+        self.outputFile = outputFile
+        self.tokenizer = tokenizer
+        self.depth = 0  # incr for {, decr for }
+        self.depthStack = []  # append for {, pop for }
+        self.compileClass()
 
     def compileClass(self):
-        # non-terminal
-        pass
+        self.depthStack.append(ET.Element("class"))
+        while self.tokenizer.hasMoreTokens():
+            # =========================================================
+            self.tokenizer.advance()
+            # =========================================================
+            if self.tokenizer.tokenType() == "KEYWORD":
+                # --------------------------------------------------
+                if self.tokenizer.keyWord() == "class":
+                    ET.SubElement(self.depthStack[-1], "keyword").text = (
+                        self.tokenizer.keyWord()
+                    )
+                # --------------------------------------------------
+                elif (
+                    self.tokenizer.keyWord() == "constructor"
+                    or self.tokenizer.keyWord() == "method"
+                    or self.tokenizer.keyWord() == "function"
+                ):
+                    self.compileSubroutine()
+                # --------------------------------------------------
+                elif (
+                    self.tokenizer.keyWord() == "int"
+                    or self.tokenizer.keyWord() == "boolean"
+                    or self.tokenizer.keyWord() == "char"
+                ):
+                    # TERMINAL BOYS!!
+                    ET.SubElement(self.depthStack[-1], "keyword").text = (
+                        self.tokenizer.keyWord()
+                    )
+                # --------------------------------------------------
+                elif self.tokenizer.keyWord() == "VOID":
+                    pass
+                # --------------------------------------------------
+                elif self.tokenizer.keyWord() == "VAR":
+                    pass
+                # --------------------------------------------------
+                elif (
+                    self.tokenizer.keyWord() == "static"
+                    or self.tokenizer.keyWord() == "field"
+                ):
+                    self.compileClassVarDec()
+                # --------------------------------------------------
+                elif self.tokenizer.keyWord() == "LET":
+                    pass
+                # --------------------------------------------------
+                elif self.tokenizer.keyWord() == "DO":
+                    pass
+                # --------------------------------------------------
+                elif self.tokenizer.keyWord() == "IF":
+                    pass
+                # --------------------------------------------------
+                elif self.tokenizer.keyWord() == "ELSE":
+                    pass
+                # --------------------------------------------------
+                elif self.tokenizer.keyWord() == "WHILE":
+                    pass
+                elif self.tokenizer.keyWord() == "RETURN":
+                    pass
+                elif self.tokenizer.keyWord() == "TRUE":
+                    pass
+                elif self.tokenizer.keyWord() == "FALSE":
+                    pass
+                elif self.tokenizer.keyWord() == "NULL":
+                    pass
+                elif self.tokenizer.keyWord() == "THIS":
+                    pass
+            # =========================================================
+            elif self.tokenizer.tokenType() == "SYMBOL":
+                # TODO:
+                # this feels like an append with {
+                # this feels like a .pop() with ; and }
+                # also gotta handle , and shit
+                ET.SubElement(self.depthStack[-1], "symbol").text = (
+                    self.tokenizer.symbol()
+                )
+                if (
+                    self.tokenizer.symbol() == ";" or self.tokenizer.symbol() == "}"
+                ) and len(self.depthStack > 1):
+                    # done with whatever statement we were parsing
+                    self.depthStack.pop()
+
+            # =========================================================
+            elif self.tokenizer.tokenType() == "INT_CONST":
+                pass
+            # =========================================================
+            elif self.tokenizer.tokenType() == "STRING_CONST":
+                pass
+            # =========================================================
+            elif self.tokenizer.tokenType() == "IDENTIFIER":
+                ET.SubElement(self.depthStack[-1], "identifier").text = (
+                    self.tokenizer.identifier()
+                )
+            # =========================================================
+
+        tree = ET.ElementTree(self.depthStack[-1])
+        ET.indent(tree, "")
+        tree.write(self.outputFile)
 
     def compileClassVarDec(self):
-        # non-terminal
-        pass
+        self.depthStack.append(ET.SubElement(self.depthStack[-1], "classVarDec"))
+        # i.e. field // static
+        ET.SubElement(self.depthStack[-1], "keyword").text = self.tokenizer.keyWord()
 
     def compileSubroutine(self):
         # non-terminal
-        pass
+        self.depthStack.append(ET.SubElement(self.depthStack[-1], "subroutineDec"))
+        # i.e. constructor // function // method
+        ET.SubElement(self.depthStack[-1], "keyword").text = self.tokenizer.keyWord()
 
     def compileParameterList(self):
         # non-terminal
