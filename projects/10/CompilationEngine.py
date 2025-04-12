7a3854b2f0cbe1cc3259c17dea78ffa6a5badcaa
Ludvig Snihs <ludvig.snihs@outlook.com>
2025-04-12 13:54:45 +0200

    Add initial support for all methods. Add a few helper methods

--- projects/10/CompilationEngine.py
+++ projects/10/CompilationEngine.py
@@ -35,10 +35,10 @@ class CompilationEngine:
             )
         else:
             # <keyword> class </keyword>
-            ET.SubElement(self.root, self.token.tag).text = self.token.text
+            self.writexml(self.root)
         while (self.token.tag != "symbol") and (self.token.text != "{"):
             self.advance()
-            ET.SubElement(self.root, self.token.tag).text = self.token.text
+            self.writexml(self.root)
         ### =============================
         # Recursive Part!!!
         while (self.token.tag == "keyword") and (
@@ -53,7 +53,7 @@ class CompilationEngine:
             self.advance()
         ### =============================
         # Write <symbol> } </symbol>
-        ET.SubElement(self.root, self.token.tag).text = self.token.text
+        self.writexml(self.root)
 
         # Write output file
         tree = ET.ElementTree(self.root)
@@ -62,78 +62,281 @@ class CompilationEngine:
 
     def compileClassVarDec(self):
         branch = ET.SubElement(self.root, "classVarDec")
-        ET.SubElement(branch, self.token.tag).text = self.token.text
+        self.writexml(branch)
         while (self.token.tag != "symbol") and (self.token.text != ";"):
             self.advance()
-            ET.SubElement(branch, self.token.tag).text = self.token.text
+            self.writexml(branch)
 
     def compileSubroutine(self):
-        branch = ET.SubElement(self.root, "subroutineDec")
-        ET.SubElement(branch, self.token.tag).text = self.token.text
+        branchDec = ET.SubElement(self.root, "subroutineDec")
+        self.writexml(branchDec)
         while (self.token.tag != "symbol") and (self.token.text != "("):
             self.advance()
-            ET.SubElement(branch, self.token.tag).text = self.token.text
-        if (self.token.tag == "symbol") and (self.token.text == "("):
-            ET.SubElement(branch, self.token.tag).text = self.token.text
-            self.advance()
-            self.compileParameterList(branch)
-        else:
-            raise Exception(
-                f"Expected ' <symbol> ( </symbol>' but got '<"
-                + self.token.tag
-                + "> "
-                + self.token.text
-                + " </"
-                + self.token.tag
-                + ">'"
-            )
+            self.writexml(branchDec)
+        self.verify("symbol", "(")
+        self.writexml(branchDec)  # (
+        self.advance()
+        self.compileParameterList(branchDec)
+        self.writexml(branchDec)  # )
+        # subroutineBody
+        branchBody = ET.SubElement(self.root, "subroutineBody")
+        self.advance()
+        self.verify("symbol", "{")
+        self.writexml(branchBody)  # {
+        self.advance()
+        ## varDec*
+        while (self.token.tag == "keyword") and (self.token.text == "var"):
+            self.compileVarDec(branchBody)
+        ## statements
+        while (self.token.tag != "symbol") and (self.token.text != "}"):
+            self.compileStatements(branchBody)
+        # '}'
+        self.verify("symbol", "}")
+        self.writexml(branchBody)
 
     def compileParameterList(self, branchName):
         branch = ET.SubElement(branchName, "parameterList")
-        pass
+        while (self.token.tag != "symbol") and (self.token.text != ")"):
+            self.writexml(branch)
+            self.advance()
 
-    def compileVarDec(self):
-        # non-terminal
-        pass
+    def compileVarDec(self, branchName):
+        branch = ET.SubElement(branchName, "varDec")
+        self.writexml(branch)  # var
+        while (self.token.tag != "symbol") and (self.token.text != ";"):
+            self.advance()
+            self.writexml(branch)
+        # Last advance to get 'var' again
+        self.advance()
 
-    def compileStatements(self):
-        # non-terminal
-        pass
+    def compileStatements(self, branch):
+        # EACH CASE WILL HAVE 1 LAST ADVANCE()
+        # let
+        if (self.token.tag == "keyword") and (self.token.text == "let"):
+            self.compileLet(ET.SubElement(branch, "letStatement"))
+        # if
+        elif (self.token.tag == "keyword") and (self.token.text == "if"):
+            self.compileIf(ET.SubElement(branch, "ifStatement"))
+        # while
+        elif (self.token.tag == "keyword") and (self.token.text == "while"):
+            self.compileWhile(ET.SubElement(branch, "whileStatement"))
+        # do
+        elif (self.token.tag == "keyword") and (self.token.text == "do"):
+            # TODO
+            self.compileDo(ET.SubElement(branch, "doStatement"))
+        # return
+        elif (self.token.tag == "keyword") and (self.token.text == "return"):
+            # TODO
+            self.compileReturn(ET.SubElement(branch, "returnStatement"))
+        else:
+            self.verify("keyword", "ANY")
 
-    def compileDo(self):
-        # non-terminal
-        pass
+    def compileDo(self, branch):
+        # do
+        self.writexml(branch)
+        # id
+        self.advance()
+        self.writexml(branch)
+        # symbol
+        self.advance()
+        self.writexml(branch)
+        if (self.token.tag == "symbol") and (self.token.text == "("):
+            # function/method of current class
+            self.compileExpressionList(ET.SubElement(branch, "expressionList"))
+            self.verify("symbol", ")")
+            self.writexml(branch)
+        elif (self.token.tag == "symbol") and (self.token.text == "."):
+            # function/method of other class
+            self.advance()
+            self.writexml(branch)  # sub-id
+            self.advance()
+            self.verify("symbol", "(")
+            self.writexml(branch)
+            self.compileExpressionList(ET.SubElement(branch, "expressionList"))
+            self.verify("symbol", ")")
+            self.writexml(branch)
+        self.advance()
 
-    def compileLet(self):
-        # non-terminal
-        pass
+    def compileLet(self, branch):
+        # let
+        self.writexml(branch)
+        # varName
+        self.advance()
+        self.writexml(branch)
+        # '[' or '='
+        self.advance()
+        self.writexml(branch)
+        if (self.token.tag == "symbol") and (self.token.text == "["):
+            # Expression
+            branchExpression = ET.SubElement(branch, "expression")
+            self.compileExpression(branchExpression)
+            self.verify("symbol", "]")
+            self.writexml(branch)  # ']'
+            # '='
+            self.advance()
+            self.verify("symbol", "=")
+            self.writexml(branch)
+            self.compileExpression(branchExpression)
+        elif (self.token.tag == "symbol") and (self.token.text == "="):
+            branchExpression = ET.SubElement(branch, "expression")
+            self.compileExpression(branchExpression)
+        self.verify("symbol", ";")
+        self.writexml(branch)
+        self.advance()
 
-    def compileWhile(self):
-        # non-terminal
-        pass
+    def compileWhile(self, branch):
+        # while
+        self.writexml(branch)
+        # symbol
+        self.advance()
+        self.verify("symbol", "(")
+        self.writexml(branch)
+        # expression
+        self.compileExpression(ET.SubElement(branch, "expression"))
+        # symbol
+        self.verify("symbol", ")")
+        self.writexml(branch)
+        # symbol
+        self.advance()
+        self.verify("symbol", "{")
+        self.writexml(branch)
+        # statements
+        self.advance()
+        self.compileStatements(ET.SubElement(branch, "statements"))
+        self.verify("symbol", "}")
+        self.writexml(branch)
+        self.advance()
 
-    def compileReturn(self):
-        # non-terminal
-        pass
+    def compileReturn(self, branch):
+        # return
+        self.writexml(branch)
+        if (self.peek().tag != "symbol") and (self.peek().text != ";"):
+            self.compileExpression(ET.SubElement(branch, "expression"))
+            self.verify("symbol", ";")
+            self.writexml(branch)
+        else:
+            self.advance()
+            self.verify("symbol", ";")
+            self.writexml(branch)
+        self.advance()
 
-    def compileIf(self):
-        # non-terminal
-        pass
+    def compileIf(self, branch):
+        # if
+        self.writexml(branch)
+        # symbol
+        self.advance()
+        self.verify("symbol", "(")
+        self.writexml(branch)
+        # expression
+        self.compileExpression(ET.SubElement(branch, "expression"))
+        # symbol
+        self.verify("symbol", ")")
+        self.writexml(branch)
+        # symbol
+        self.advance()
+        self.verify("symbol", "{")
+        self.writexml(branch)
+        # statements
+        # TODO: probably one advance here
+        self.advance()
+        self.compileStatements(ET.SubElement(branch, "statements"))
+        self.verify("symbol", "}")
+        self.writexml(branch)
+        self.advance()
+        if (self.token.tag == "keyword") and (self.token.text == "else"):
+            # 'else' '{'
+            for _ in range(2):
+                self.writexml(branch)
+                self.advance()
+            self.compileStatements(ET.SubElement(branch, "statements"))
+            self.verify("symbol", "}")
+            self.writexml(branch)
+            self.advance()
 
-    def compileExpression(self):
-        # non-terminal
-        pass
+    def compileExpression(self, branch):
+        self.advance()  # term
+        branchTerm = ET.SubElement(branch, "term")
+        self.compileTerm(branchTerm)
+        if self.token.text in ("+", "-", "*", "/", "&", "|", "<", ">", "="):
+            self.writexml(branch)
+            self.compileExpression(branch)
 
-    def compileTerm(self):
-        # non-terminal
-        # Lookahead!!
-        pass
+    def compileTerm(self, branch):
+        if self.token.tag == "integerConstant":
+            self.writexml(branch)
+        elif self.token.tag == "stringConstant":
+            self.writexml(branch)
+        elif (self.token.tag == "keyword") and (
+            (self.token.text) in ("true", "false", "null", "this")
+        ):
+            self.writexml(branch)
+        elif self.token.tag == "identifier":
+            # LOOKAHEAD!!! need to diff between varName, varName[], and varName.xxx()
+            lookaheadToken = self.peek()
+            if (lookaheadToken.tag == "symbol") and (lookaheadToken.text == "["):
+                # array
+                self.writexml(branch)
+                self.advance()
+                self.verify("symbol", "[")
+                self.writexml(branch)
+                self.compileExpression(branch)
+                self.verify("symbol", "]")
+                self.writexml(branch)
+            elif (lookaheadToken.tag == "symbol") and (lookaheadToken.text == "("):
+                # function/method of current class
+                self.writexml(branch)  # id
+                self.advance()
+                self.verify("symbol", "(")
+                self.writexml(branch)
+                self.compileExpressionList(ET.SubElement(branch, "expressionList"))
+                self.verify("symbol", ")")
+                self.writexml(branch)
+            elif (lookaheadToken.tag == "symbol") and (lookaheadToken.text == "."):
+                # function/method of other class
+                self.writexml(branch)  # id
+                self.advance()
+                self.verify("symbol", ".")
+                self.writexml(branch)  # '.'
+                self.advance()
+                self.writexml(branch)  # sub-id
+                self.advance()
+                self.verify("symbol", "(")
+                self.writexml(branch)
+                self.compileExpressionList(ET.SubElement(branch, "expressionList"))
+                self.verify("symbol", ")")
+                self.writexml(branch)
+            else:
+                # varName
+                self.writexml(branch)
+        elif (self.token.tag == "symbol") and (self.token.text == "("):
+            # '(' expression ')'
+            self.writexml(branch)
+            self.advance()
+            self.compileExpression(branch)
+            self.verify("symbol", ")")
+            self.writexml(branch)
+        elif (self.token.tag == "symbol") and (
+            (self.token.text == "~") or (self.token.text == "-")
+        ):
+            # UNARY OP
+            self.writexml(branch)
+            self.advance()
+            branchTerm = ET.SubElement(branch, "term")
+            self.compileTerm(branchTerm)
+        # Last advance
+        self.advance()
 
-    def compileExpressionList(self):
-        # non-terminal
-        pass
+    def compileExpressionList(self, branch):
+        """Compiles a list of 0 or more expressions. \n
+        Grammar: (expression (',' expression)*)?"""
+        branchExpression = ET.SubElement(branch, "expression")
+        self.compileExpression(branchExpression)
+        if (self.token.tag == "symbol") and (self.token.text == ","):
+            self.writexml(branch)
+            self.compileExpression(branchExpression)
 
-    ## Helper functions
+    ## Helper functions ##
+
     def hasMoreTokens(self):
         """Checks if more tokens are available.
         Returns true if current idx is smaller than length of
@@ -146,6 +349,27 @@ class CompilationEngine:
             self.token = self.tokens[self.idx]
         else:
             self.token = None
+
+    def writexml(self, branch):
+        ET.SubElement(branch, self.token.tag).text = self.token.text
+
+    def verify(self, expectedTag: str, expectedText: str):
+        if (self.token.tag != expectedTag) or (self.token.text != expectedText):
+            raise Exception(
+                f"Expected '<"
+                + expectedTag
+                + "> "
+                + expectedText
+                + " </"
+                + expectedTag
+                + ">' but got '<"
+                + self.token.tag
+                + "> "
+                + self.token.text
+                + " </"
+                + self.token.tag
+                + ">'"
+            )
 
     def peek(self):
         if self.hasMoreTokens():
